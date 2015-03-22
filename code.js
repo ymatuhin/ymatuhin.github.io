7701beaf63054ce07c4b48ec84dbcf1b06563ad4
Yury Matuhin <ymatuhin@yandex.ru>
2015-03-22 17:07:42 +0300

    upd| new version with jekyll

--- code.js
+++
@@ -1,3 +0,0 @@
-var el = document.getElementsByTagName('body')[0];
-el.innerHTML = el.innerHTML.replace(/«/g, '<span class="slaquo"> </span> <span class="hlaquo"> «</span>')
-el.innerHTML = el.innerHTML.replace(/\(/g, '<span class="sbrace"> </span> <span class="hbrace"> (</span>')
