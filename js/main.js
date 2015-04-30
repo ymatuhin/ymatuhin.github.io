7daec12d115adf2dde98692edfc192ff92d8d917
Yury Matuhin <ymatuhin@yandex.ru>
2015-04-30 18:47:29 +0300

    [upd] remove dark mode and add new post

--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
-require(['instantclick.min', 'typo', 'disqus', 'share42', 'dom-classes'], function(ic, typo, disqus, share42, cls) {
+require(['instantclick.min', 'typo', 'disqus', 'share42'], function(ic, typo, disqus, share42) {
 	InstantClick.init();
 	InstantClick.on('change', onAjaxLoadFn);
-	InstantClick.on('wait', function () {
-		cls.add(document.documentElement, 'wait');
-	});
 
 	disqus.init();
 	onPageReady();
 
 	function onAjaxLoadFn () {
-		cls.add(document.documentElement, 'wait');
-		setTimeout(function () {
-			cls.remove(document.documentElement, 'wait');
-		}, 500);
-
 		onPageReady();
-
 		// Try reload metrica
 		try { window.yaCounter28017147.hit(location.href); } catch(e) { }
 		try { window.pluso.refreshCounter(location.href); } catch(e) { }
@@ -26,14 +17,6 @@ require(['instantclick.min', 'typo', 'disqus', 'share42', 'dom-classes'], functi
 		typo.go();
 		share42.init();
 	}
-});
-
-
-require(['instantclick.min', 'dark-mode'], function(ic, dark) {
-	dark.init();
-	InstantClick.on('change', function () {
-		dark.init();
-	});
 });
 
 require(['yaMetrica'], function(metrica) {
