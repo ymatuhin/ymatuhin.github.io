59698318cec0a4fb5e22c69c0dcf3520d18fbc0c
ymatuhin <ymatuhin@yandex.ru>
2015-06-24 00:08:57 +0300

    [add] instantclick

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,21 +1,12 @@
-require(['instantclick.min', 'typo', 'disqus', 'share42'], function(ic, typo, disqus, share42) {
+require(['instantclick.min', 'disqus'], function(ic, disqus) {
+	disqus.init();
 	InstantClick.init();
 	InstantClick.on('change', onAjaxLoadFn);
 
-	disqus.init();
-	onPageReady();
-
 	function onAjaxLoadFn () {
-		onPageReady();
 		// Try reload metrica
 		try { window.yaCounter28017147.hit(location.href); } catch(e) { }
-		try { window.pluso.refreshCounter(location.href); } catch(e) { }
 		setTimeout(disqus.reload, 1000);
-	}
-
-	function onPageReady () {
-		typo.go();
-		share42.init();
 	}
 });
 
@@ -23,6 +14,6 @@ require(['yaMetrica'], function(metrica) {
 	metrica.init();
 });
 
-require(['cssLoader'], function(cssLoader) {
-	cssLoader.load('/css/secondary.css');
-});
+// require(['cssLoader'], function(cssLoader) {
+// 	cssLoader.load('/css/secondary.css');
+// });
