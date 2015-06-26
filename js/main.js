f24670cfdb14b13e29b38a0aba4ac5a00a319509
ymatuhin <ymatuhin@yandex.ru>
2015-06-26 09:08:54 +0300

    [upd] style

--- js/main.js
+++ js/main.js
@@ -3,22 +3,10 @@ require(['instantclick.min', 'disqus'], function(ic, disqus) {
 	InstantClick.init();
 	InstantClick.on('change', onAjaxLoadFn);
 
-	removeNoscript();
-
 	function onAjaxLoadFn () {
-		removeNoscript();
-
 		// Try reload metrica
 		try { window.yaCounter28017147.hit(location.href); } catch(e) { }
 		setTimeout(disqus.reload, 1000);
-	}
-
-	function removeNoscript() {
-		var html = document.getElementsByTagName('body')[0];
-
-		if (html.innerHTML.indexOf('<noscript') >= 0) {
-			html.innerHTML = html.innerHTML.replace( /<noscript[\S\s]*?<\/noscript>/ig, '');
-		}
 	}
 });
 
