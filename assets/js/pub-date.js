6d0e23038c5600d1f01f1886987746a778e87a00
ymatuhin <ymatuhin@yandex.ru>
2016-03-15 16:58:18 +0300

    [upd] pubdate js

--- assets/js/pub-date.js
+++ assets/js/pub-date.js
@@ -1,36 +1,39 @@
-var dateMod = document.querySelector('meta[itemprop="dateModified"]');
-var datePub = document.querySelector('meta[itemprop="datePublished"]');
+(function () {
+	var dateMod = document.querySelector('meta[itemprop="dateModified"]');
+	var datePub = document.querySelector('meta[itemprop="datePublished"]');
 
-if (!dateMod || !datePub) return;
-var mod  = new Date(dateMod.getAttribute('content'));
-var pub  = new Date(datePub.getAttribute('datetime'));
+	if (!dateMod || !datePub) return;
 
-var days = Math.floor((new Date() - mod)/(1000*60*60*24));
-var updText = document.querySelector('.js-updated');
+	var mod  = new Date(dateMod.getAttribute('content'));
+	var pub  = new Date(datePub.getAttribute('datetime'));
 
-function declOfNum(number, titles) {
-	cases = [2, 0, 1, 1, 1, 2];
-	return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
-}
+	var days = Math.floor((new Date() - mod)/(1000*60*60*24));
+	var updText = document.querySelector('.js-updated');
 
-if (mod > pub) {
-	var _txt = updText.innerText || updText.textContent;
-	var shownText = '';
-
-	if (days == 0) {
-		shownText = 'сегодня';
-	} else if (days == 1) {
-		shownText = 'вчера';
-	} else if (days > 0 && days < 30) {
-		shownText = days + ' ' + declOfNum(days, ['день', 'дня', 'дней']) + ' назад';
-	} else if (days > 30 && days < 365) {
-		shownText = Math.floor(days/30) + ' ' + declOfNum(Math.floor(days/30), ['месяц', 'месяца', 'месяцев']) + ' назад';
-	} else if (days > 365) {
-		shownText = Math.floor(days/365) + ' ' + declOfNum(Math.floor(days/365), ['год', 'года', 'лет']) + ' назад';
+	function declOfNum(number, titles) {
+		cases = [2, 0, 1, 1, 1, 2];
+		return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
 	}
 
-	if (days >= 0) {
-		updText.style.display = '';
-		updText.innerHTML = _txt + ' <em>' + shownText + '</em>.';
+	if (mod > pub) {
+		var _txt = updText.innerText || updText.textContent;
+		var shownText = '';
+
+		if (days == 0) {
+			shownText = 'сегодня';
+		} else if (days == 1) {
+			shownText = 'вчера';
+		} else if (days > 0 && days < 30) {
+			shownText = days + ' ' + declOfNum(days, ['день', 'дня', 'дней']) + ' назад';
+		} else if (days > 30 && days < 365) {
+			shownText = Math.floor(days/30) + ' ' + declOfNum(Math.floor(days/30), ['месяц', 'месяца', 'месяцев']) + ' назад';
+		} else if (days > 365) {
+			shownText = Math.floor(days/365) + ' ' + declOfNum(Math.floor(days/365), ['год', 'года', 'лет']) + ' назад';
+		}
+
+		if (days >= 0) {
+			updText.style.display = '';
+			updText.innerHTML = _txt + ' <em>' + shownText + '</em>.';
+		}
 	}
-}
+})();
