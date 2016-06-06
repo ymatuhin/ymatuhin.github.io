b2e9552393549d1f907656805c82802646f435b4
ymatuhin <ymatuhin@yandex.ru>
2016-06-06 12:56:55 +0300

    Add better critical css

--- assets/js/emoji.js
+++
@@ -1,9 +0,0 @@
-window.doesSupportEmoji=function(){var a;if(document.createElement("canvas").getContext&&(a=document.createElement("canvas").getContext("2d"),"function"==typeof a.fillText))return smile=String.fromCodePoint(128516),a.textBaseline="top",a.font="32px Arial",a.fillText(smile,0,0),0!==a.getImageData(16,16,1,1).data[0]};
-var isMacLike = navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i)?true:false;
-if (!doesSupportEmoji() || !isMacLike) {
-    var script = document.createElement('script');
-    script.onload = function () { twemoji.parse(document.querySelector('.text-content')) };
-    script.setAttribute("type","text/javascript");
-    script.setAttribute("src", "//twemoji.maxcdn.com/2/twemoji.min.js");
-    document.getElementsByTagName("head")[0].appendChild(script);
-}
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,27 +1,106 @@
 'use strict';
 
 import gulp from 'gulp';
+import csso from 'gulp-csso';
+import exec from 'gulp-exec';
+import concat from 'gulp-concat';
+import cssPurge from 'gulp-css-purge';
+import purifycss from 'gulp-purifycss';
+const  critical = require('critical').stream;
 
-// todo
-add autoprefixer here and imageoptim
-generate css form all sass -> as parametr for critical (remove original props from css) -> recursive
-gulp shell -> critical all html files (exclude /demo/) -> concat css -> gulp-css-purge -> inline to all html
-localStorage font loading
+const criticalConfig = {
+	base: '_site/',
+	// minify: true,
+	// inline: true,
+	extract: true,
+	dimensions: [{
+		width: 1300,
+		height: 900
+	}, {
+		width: 480,
+		height: 600
+	}, {
+		width: 800,
+		height: 800
+	}],
+	css: ['assets/css/main.min.css']
+}
 
-gulp.task('critical', () => {
-	gulp.src('_site/**/*.html')
-		.pipe()
-		.pipe(gulp.dest('./critical'))
-	//
-    // critical.generate({
-    //     inline: true,
-    //     base: 'dist/',
-    //     src: 'index.html',
-	// 	dest: 'styles/styles.min.css',
-	//     minify: true,
-	//     width: 1300,
-	//     height: 900
-    // });
+// Generate & Inline Critical-path CSS
+
+gulp.task('сss:critical:home:generate', () => {
+	return gulp.src('_site/index.html')
+		.pipe(critical(criticalConfig))
+		.pipe(gulp.dest('.tmp/home'));
 });
 
-gulp.task('default', gulp.series('critical'));
+gulp.task('сss:critical:home', gulp.series('сss:critical:home:generate', () => {
+	return gulp.src('.tmp/home/**/*.css')
+		.pipe(concat('critical-home.css'))
+		.pipe(csso())
+		.pipe(gulp.dest('_includes'));
+}));
+
+gulp.task('сss:critical:blog:generate', () => {
+	return gulp.src('_site/blog/index.html')
+		.pipe(critical(criticalConfig))
+		.pipe(gulp.dest('.tmp/blog'));
+});
+
+gulp.task('сss:critical:blog', gulp.series('сss:critical:blog:generate', () => {
+	return gulp.src('.tmp/blog/**/*.css')
+		.pipe(concat('critical-blog.css'))
+		.pipe(csso())
+		.pipe(gulp.dest('_includes'));
+}));
+
+gulp.task('сss:critical:post:generate', () => {
+	return gulp.src([
+		'_site/about/index.html',
+		'_site/books/index.html',
+		'_site/front-end/logs-in-javascript/index.html',
+		'_site/front-end/retina_media_quires/index.html',
+		'_site/front-end/problems_of_the_extensions/index.html',
+		'_site/blog/how_to_resign_from_the_payler_poloniumarts/index.html',
+	], { base: "_site" })
+		.pipe(critical(criticalConfig))
+		.pipe(gulp.dest('.tmp/post'));
+});
+
+gulp.task('сss:critical:post', gulp.series('сss:critical:post:generate', () => {
+	return gulp.src('.tmp/post/**/*.css')
+		.pipe(concat('critical-post.css'))
+		.pipe(cssPurge())
+		.pipe(csso())
+		.pipe(gulp.dest('_includes'));
+}));
+
+
+gulp.task('css:main', () => {
+	return gulp.src('_site/assets/css/main.css')
+		.pipe(purifycss([
+			'_site/**/*.html',
+			'_site/**/*.js'
+		]))
+		.pipe(concat('main.min.css'))
+		.pipe(csso())
+		.pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('css:critical', gulp.parallel([
+	'сss:critical:home',
+	'сss:critical:blog',
+	'сss:critical:post'
+]));
+
+gulp.task('watch', (cb) => {
+	gulp.watch('_site/assets/css/main.css', gulp.parallel('css:main', 'css:critical'));
+	cb();
+});
+
+// todo
+// add autoprefixer here and imageoptim
+// generate css form all sass -> as parametr for critical (remove original props from css) -> recursive
+// gulp shell -> critical all html files (exclude /demo/) -> concat css -> gulp-css-purge -> inline to all html
+// localStorage font loading
+gulp.task('default', gulp.series('css:main', 'css:critical'));
